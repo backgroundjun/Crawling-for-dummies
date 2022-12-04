e561130ca4a778a6fa9121bdf5cac7da791cb4ad
eli <gjbae0@gmail.com>
2022-12-05 03:26:55 +0900

    feat: 클릭 시 selector 가져오기

--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,14 @@
-console.log("This is a popup!");
+document.addEventListener('DOMContentLoaded', function () {
+  var checkButton = document.getElementById('check');
+  checkButton.addEventListener('click', function () {
+    alert("Hey your button is working!");
+  }, false);
+}, false);
+
+document.getElementById("check").onclick = function () {
+  chrome.runtime.sendMessage('START', (response) => {
+    // 3. Got an asynchronous response with the data from the service worker
+    console.log('received user data', response);
+    initializeUI(response);
+  });
+};
